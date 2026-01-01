6d59ea7acf9f3b35d520ced12e3f42db9685093a
Dennis <denniskostjuk@gmail.com>
2026-01-01 19:34:43 +0100

    lightmode new bg color

--- components/effects/LiquidBackground.tsx
+++ components/effects/LiquidBackground.tsx
@@ -3,7 +3,7 @@
 export default function LiquidBackground() {
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none textured-surface">
-      {/* Statischer Hintergrund: Schwarz im Darkmode, Weiß im Lightmode */}
+      {/* Statischer Hintergrund: Schwarz im Darkmode, #E2D7CE im Lightmode */}
       <div className="absolute inset-0 bg-background transition-colors duration-500" />
     </div>
   );
