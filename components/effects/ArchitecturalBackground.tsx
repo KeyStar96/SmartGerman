36f6eb7d12c5e63e59fc8c9aa7d4fa6d78f38975
Dennis <denniskostjuk@gmail.com>
2026-01-06 07:07:11 +0100

    bg

--- components/effects/ArchitecturalBackground.tsx
+++ components/effects/ArchitecturalBackground.tsx
@@ -7,7 +7,7 @@ export default function ArchitecturalBackground() {
         <div className="fixed inset-0 overflow-hidden pointer-events-none select-none z-0">
             <div className="absolute inset-0 w-full h-full transition-opacity duration-700 ease-in-out opacity-100 dark:opacity-0">
                 <Image
-                    src="/Bilder/SG_LP_BG_Light.png"
+                    src="/Bilder/Test_BG_Light.png"
                     alt="Architectural Background Light"
                     fill
                     priority
