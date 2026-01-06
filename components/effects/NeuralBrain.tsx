0fc91ed655b85c2741866692d73a7764a3d95fbb
Dennis <denniskostjuk@gmail.com>
2026-01-06 16:36:27 +0100

    brain opitimizaion

--- components/effects/NeuralBrain.tsx
+++ components/effects/NeuralBrain.tsx
@@ -6,7 +6,7 @@ import * as THREE from "three";
 
 // --- CONFIG ---
 const CONFIG = {
-    neuronDensity: 7,
+    neuronDensity: 10,
     connectionDistance: 0.6, // Reduced for local connections only
     minConnectionDistance: 0.15, // Avoid tiny micro-connections
     wanderRadius: 0.2,       // Increased for visible movement
