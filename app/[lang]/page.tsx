b4d1cbb7eebb33048d48fb2e0003fab603eea834
Dennis <denniskostjuk@gmail.com>
2026-01-23 17:50:57 +0100

    Anmeldeseite

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -9,6 +9,7 @@ import FooterLayout from "@/components/footer/FooterLayout";
 import { getDictionary } from "@/lib/dictionary";
 import ArchitecturalBackground from "@/components/effects/ArchitecturalBackground";
 import BackgroundPinner from "@/components/effects/BackgroundPinner";
+import RegistrationWizard from "@/components/sections/RegistrationWizard";
 
 export default async function HomePage({
   params,
@@ -48,6 +49,7 @@ export default async function HomePage({
           <AboutContainer dictionary={dictionary} />
           <WhyUsBento dictionary={dictionary} />
           <Courses dictionary={dictionary} />
+          <RegistrationWizard />
           <TimetableSection dictionary={dictionary} />
           <LocationSection dictionary={dictionary} />
 
