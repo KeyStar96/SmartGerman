b866ea6a6ecb5e6cfe6614b37ddd25611d58ce9c
Dennis <denniskostjuk@gmail.com>
2026-01-23 19:02:58 +0100

    neu Anmeldeseite

--- app/[lang]/registration/page.tsx
+++ app/[lang]/registration/page.tsx
@@ -50,7 +50,14 @@ export default async function RegistrationPage({
                         </header>
 
                         <div className="md:pl-20">
-                            <EnrollmentForm />
+                            <React.Suspense fallback={
+                                <div className="animate-pulse space-y-4">
+                                    <div className="h-8 bg-gray-200 rounded w-1/3"></div>
+                                    <div className="h-4 bg-gray-100 rounded w-2/3"></div>
+                                </div>
+                            }>
+                                <EnrollmentForm />
+                            </React.Suspense>
                         </div>
                     </div>
                 </div>
