7591281f1ef86c298e9ee6e7d9c8e4118c9f27f0
Dennis <denniskostjuk@gmail.com>
2026-01-24 15:07:21 +0100

    dynamische preisberechnung

--- app/[lang]/registration/template.tsx
+++ app/[lang]/registration/template.tsx
@@ -4,10 +4,10 @@ import { motion } from "framer-motion";
 export default function Template({ children }: { children: React.ReactNode }) {
     return (
         <motion.div
-            initial={{ x: "100%", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: "100%", opacity: 0 }}
-            transition={{ type: "spring", stiffness: 100, damping: 20, mass: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
             className="absolute inset-0 w-full h-full bg-[#F0EFE9] z-50 pointer-events-auto"
         >
             {children}
