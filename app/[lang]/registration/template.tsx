419fed994a6a4a4105623e27ee818e11d8f6d689
Dennis <denniskostjuk@gmail.com>
2026-01-25 20:16:35 +0100

    unittests

--- app/[lang]/registration/template.tsx
+++ app/[lang]/registration/template.tsx
@@ -1,16 +1,5 @@
 "use client";
-import { motion } from "framer-motion";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-            className="absolute inset-0 w-full h-full bg-[#F0EFE9] z-50 pointer-events-auto"
-        >
-            {children}
-        </motion.div>
-    );
+    return <>{children}</>;
 }
