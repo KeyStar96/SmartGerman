f7f9545385a0834551070708a7bed3ce018cf65c
Dennis <denniskostjuk@gmail.com>
2026-01-30 21:51:34 +0100

    mobile footer

--- components/footer/FooterLayout.tsx
+++ components/footer/FooterLayout.tsx
@@ -15,7 +15,7 @@ export default function FooterLayout({ dictionary }: FooterLayoutProps) {
           - z-0 ensures it is behind the relative z-10 main content
           - No spacer needed here because page.tsx creates the window via margin-bottom
         */
-        <footer className="fixed bottom-0 left-0 w-full h-[1100px] md:h-[500px] z-0 flex flex-col justify-end overflow-hidden bg-[#050505] text-[#E2D7CE]">
+        <footer className="fixed bottom-0 left-0 w-full h-[1250px] md:h-[500px] z-0 flex flex-col justify-end overflow-hidden bg-[#050505] text-[#E2D7CE]">
 
             {/* --- AMBIENT LIGHTING (BRAND UPDATED) --- */}
             {/* --- AMBIENT LIGHTING (BRAND UPDATED) --- */}
