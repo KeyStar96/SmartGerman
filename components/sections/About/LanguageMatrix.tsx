d4094a4b3103673ea986a6880cdc47e5e91126b8
Dennis <denniskostjuk@gmail.com>
2026-02-13 20:16:23 +0100

    mobile opti

--- components/sections/About/LanguageMatrix.tsx
+++ components/sections/About/LanguageMatrix.tsx
@@ -65,7 +65,7 @@ export default function LanguageMatrix({ title, items }: LanguageMatrixProps) {
                 {title}
             </h3>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div className="grid grid-cols-2 gap-4">
                 {items.map((item, i) => (
                     <LanguageCard key={i} item={item} index={i} />
                 ))}
